48b918702046f2069d5c0a88b8e3378f0cb0da1c
steveoh <sgourley@utah.gov>
2016-05-03 18:21:52 -0600

    create console project

---
+++ uic-etl/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uic_etl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
