a218454aec7a37f4a3c40cd12cd6a759c183e27e
steveoh <sgourley@utah.gov>
2016-05-04 18:32:40 -0600

    add header information view model

---
+++ uic-etl/models/dtos/HeaderInformation.cs
@@ -0,0 +1,9 @@
+﻿namespace uic_etl.models.dtos
+{
+    public class HeaderInformation
+    {
+        public string Title { get; set; }
+        public string CreationTime { get; set; }
+        public string Comments { get; set; }
+    }
+}
