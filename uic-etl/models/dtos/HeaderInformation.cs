15cc352bfbb7ee1a6681e7fbf1e95bf9630cfa36
steveoh <sgourley@utah.gov>
2016-05-05 11:38:34 -0600

    add some documentation/sample values

--- uic-etl/models/dtos/HeaderInformation.cs
+++ uic-etl/models/dtos/HeaderInformation.cs
@@ -2,8 +2,13 @@
 {
     public class HeaderInformation
     {
+        // The header Title. DATA SUBMISSION FOR QUARTER #1 FY 2010
         public string Title { get; set; }
+
+        // The time the etl tool is run. 2010-03-24T16:22:08
         public string CreationTime { get; set; }
+
+        // Comments that go in the header section. THIS IS A SAMPLE XML DOC V.2
         public string Comments { get; set; }
     }
 }
