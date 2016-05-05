bedf461d60b47de15343984adf0eca40e8eecd51
steveoh <sgourley@utah.gov>
2016-05-05 12:25:38 -0600

    add find index by field command/model

---
+++ uic-etl/commands/FindIndexByFieldNameCommand.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using ESRI.ArcGIS.Geodatabase;
+using uic_etl.models.dtos;
+
+namespace uic_etl.commands
+{
+    /// <summary>
+    ///     Command to find the index for each attribute name
+    /// </summary>
+    public class FindIndexByFieldNameCommand
+    {
+        private readonly IFields _fields;
+        private readonly IEnumerable<string> _fieldsToMap;
+        private readonly Dictionary<string, IndexFieldMap> _propertyValueIndexMap;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="FindIndexByFieldNameCommand" /> class.
+        /// </summary>
+        /// <param name="layer"> The layer. </param>
+        /// <param name="fieldsToMap">The fields to map to an index number</param>
+        public FindIndexByFieldNameCommand(IFeatureClass layer, IEnumerable<string> fieldsToMap)
+        {
+            _fieldsToMap = fieldsToMap;
+            _fields = layer.Fields;
+            _propertyValueIndexMap = new Dictionary<string, IndexFieldMap>();
+        }
+
+        /// <summary>
+        ///     code to execute when command is run. Iterates over every month and finds the index for the field in teh feature
+        ///     class
+        /// </summary>
+        public Dictionary<string, IndexFieldMap> Execute()
+        {
+            foreach (var field in _fieldsToMap)
+            {
+                _propertyValueIndexMap.Add(field,
+                                           new IndexFieldMap(GetIndexForField(field, _fields), field));
+            }
+
+            return _propertyValueIndexMap;
+        }
+
+        /// <summary>
+        ///     Gets the index for field.
+        /// </summary>
+        /// <param name="attributeName"> The attribute name. </param>
+        /// <param name="fields"> The fields. </param>
+        /// <returns> </returns>
+        private static int GetIndexForField(string attributeName, IFields fields)
+        {
+            var findField = fields.FindField(attributeName.Trim());
+
+            return findField < 0 ? fields.FindFieldByAliasName(attributeName.Trim()) : findField;
+        }
+    }
+}
