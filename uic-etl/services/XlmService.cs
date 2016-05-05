e0b95475c94a8c4e8400c164669782443b4c5ddf
steveoh <sgourley@utah.gov>
2016-05-04 18:32:19 -0600

    add xml service with tests

---
+++ uic-etl/services/XlmService.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml.Linq;
+using uic_etl.models.dtos;
+
+namespace uic_etl.services
+{
+    public static class XlmService
+    {
+        public static XDocument CreateDocument()
+        {
+            var xmlns = XNamespace.Get("http://www.exchangenetwork.net/schema/v1.0/ExchangeNetworkDocument.xsd");
+            var xsi = XNamespace.Get("http://www.w3.org/2001/XMLSchema-instance");
+            var schemaLocation = XNamespace.Get("xmlns http://www.exchangenetwork.net/schema/v1.0/ExchangeNetworkDocument.xsd");
+
+            var doc = new XDocument
+            {
+                Declaration = new XDeclaration("1.0", "UTF-8", null),
+            };
+
+            var root = new XElement(xmlns + "Document",
+                new XAttribute(xsi + "schemaLocation", schemaLocation),
+                new XAttribute(XNamespace.Xmlns + "xsi", xsi),
+                new XAttribute(XName.Get("Id"), "6a43a7c5-0510-41ff-a15d-39657d55153d"));
+
+            doc.Add(root);
+
+            return doc;
+        }
+
+        public static void AppendHeader(ref XDocument doc, HeaderInformation model)
+        {
+            doc.Root.Add(new XElement(XName.Get("Header"),
+                new XElement(XName.Get("Author"), "CANDACE CADY"),
+                new XElement(XName.Get("Organization"), "UDEQ -- UDAH DEPARTMENT OF ENVIRONMENTAL QUALITY"),
+                new XElement(XName.Get("Title"), model.Title),
+                new XElement(XName.Get("CreationTime"), model.CreationTime),
+                new XElement(XName.Get("Comment"), model.Comments),
+                new XElement(XName.Get("DataService"), "UIC"),
+                new XElement(XName.Get("ContactInfo"), "CANDACE CADY 195 NORTH 1950 WEST, SALT LAKE CITY UT 84114, (801) 536-4352"),
+                new XElement(XName.Get("Notification"), "AGRC@UTAH.GOV"),
+                new XElement(XName.Get("Sensitivity"), "UNCLASSIFIED")));
+        }
+    }
+}
---
+++ uic-etl/tests.uic-etl/XmlWritingTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq.Expressions;
+using System.Xml.Linq;
+using uic_etl.services;
+using Xunit;
+
+namespace tests.uic_etl
+{
+    public class XmlWritingTests
+    {
+        [Fact]
+        public void CreateDocument()
+        {
+            const string documentXml = "<Document xsi:schemaLocation=\"xmlns http://www.exchangenetwork.net/schema/v1.0/ExchangeNetworkDocument.xsd\" " +
+                                       "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+                                       "Id=\"6a43a7c5-0510-41ff-a15d-39657d55153d\" " +
+                                       "xmlns=\"http://www.exchangenetwork.net/schema/v1.0/ExchangeNetworkDocument.xsd\" />";
+            
+            var expected = XDocument.Parse(documentXml);
+
+            var doc = XlmService.CreateDocument();
+
+            Assert.Equal(expected.ToString(), doc.ToString());
+        }
+
+        [Fact]
+        public void CreateHeader()
+        {
+            const string documentXml = "<Header><Author>CANDACE CADY</Author>" +
+                                       "<Organization>UDEQ -- UDAH DEPARTMENT OF ENVIRONMENTAL QUALITY</Organization>" +
+                                       "<Title>DATA SUBMISSION FOR QUARTER #1, FY 2010</Title><CreationTime>2010-03-24T16:22:08</CreationTime>" +
+                                       "<Comment>THIS IS A SAMPLE XML DOC V.2</Comment><DataService>UIC</DataService>" +
+                                       "<ContactInfo>CANDACE CADY 195 NORTH 1950 WEST, SALT LAKE CITY UT 84114, (801) 536-4352</ContactInfo>" +
+                                       "<Notification>AGRC@UTAH.GOV</Notification>" +
+                                       "<Sensitivity>UNCLASSIFIED</Sensitivity></Header>";
+
+            var expected = XDocument.Parse(documentXml);
+
+            var root = new XElement(XName.Get("Header"),
+                new XElement(XName.Get("Author"), "CANDACE CADY"),
+                new XElement(XName.Get("Organization"), "UDEQ -- UDAH DEPARTMENT OF ENVIRONMENTAL QUALITY"),
+                new XElement(XName.Get("Title"), "DATA SUBMISSION FOR QUARTER #1, FY 2010"),
+                new XElement(XName.Get("CreationTime"), "2010-03-24T16:22:08"),
+                new XElement(XName.Get("Comment"), "THIS IS A SAMPLE XML DOC V.2"),
+                new XElement(XName.Get("DataService"), "UIC"),
+                new XElement(XName.Get("ContactInfo"), "CANDACE CADY 195 NORTH 1950 WEST, SALT LAKE CITY UT 84114, (801) 536-4352"),
+                new XElement(XName.Get("Notification"), "AGRC@UTAH.GOV"),
+                new XElement(XName.Get("Sensitivity"), "UNCLASSIFIED"));
+
+            Assert.Equal(expected.ToString(), root.ToString());
+        }
+    }
+}
