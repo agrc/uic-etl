41c5babb21bc414eaa5d27f55dd5cbddb562509e
steveoh <sgourley@utah.gov>
2016-07-18 17:46:11 -0600

    constituent fk

--- domain.uic-etl/sde/ConsituentClassISdeModel.cs
+++ domain.uic-etl/sde/ConsituentClassISdeModel.cs
@@ -1,11 +1,14 @@
-﻿namespace domain.uic_etl.sde
+﻿using System;
+
+namespace domain.uic_etl.sde
 {
     public class ConstituentClassISdeModel
     {
-        public static string[] Fields = { "GUID", "Concentration", "Unit", "ConstituentCode" };
+        public static string[] Fields = { "GUID", "Concentration", "Unit", "ConstituentCode", "ClassIWaste_FK" };
 
         public int Concentration { get; set; }
         public int Unit { get; set; }
         public string ConstituentCode { get; set; }
+        public Guid WasteGuid { get; set; }
     }
 }
