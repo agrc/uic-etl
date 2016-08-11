d746ca42bb7b341475ae4790997e318e9a71dc22
steveoh <sgourley@utah.gov>
2016-08-10 18:16:51 -0600

    add lookups from python code
    closes #8

--- domain.uic-etl/sde/AuthorizationSdeModel.cs
+++ domain.uic-etl/sde/AuthorizationSdeModel.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.Linq;
 
 namespace domain.uic_etl.sde
 {
     public class AuthorizationSdeModel
     {
         public static string[] Fields = { "GUID", "AuthorizeType", "OwnerSectorType", "AuthorizeNumber" };
+        private string _ownerSectorType;
 
         public string AuthorizeType { get; set; }
-        public string OwnerSectorType { get; set; }
+
+        public string OwnerSectorType
+        {
+            get
+            {
+                if (new []{ "sg", "lg"}.Contains(_ownerSectorType.ToLower()))
+                {
+                    return "OS";
+                }
+                return _ownerSectorType;
+            }
+            set { _ownerSectorType = value; }
+        }
+
         public string AuthorizeNumber { get; set; }
         public Guid Guid { get; set; }
     }
--- domain.uic-etl/sde/WellStatusSdeModel.cs
+++ domain.uic-etl/sde/WellStatusSdeModel.cs
@@ -1,14 +1,30 @@
 ﻿using System;
+using System.Linq;
 
 namespace domain.uic_etl.sde
 {
     public class WellStatusSdeModel
     {
         public static string[] Fields = {"Guid", "OperatingStatusDate", "OperatingStatusType", "Well_FK"};
+        private string _operatingStatusType;
 
         public Guid Guid { get; set; }
         public DateTime OperatingStatusDate { get; set; }
-        public string OperatingStatusType { get; set; }
+
+        public string OperatingStatusType
+        {
+            get
+            {
+                if (new[] {"ot", "pr"}.Contains(_operatingStatusType.ToLower()))
+                {
+                    return "UC";
+                }
+
+                return _operatingStatusType;
+            }
+            set { _operatingStatusType = value; }
+        }
+
         public Guid WellGuid { get; set; }
     }
 }
