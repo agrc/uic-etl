4304bb4098c44a6f47d96d0b75c043515517c4bf
steveoh <sgourley@utah.gov>
2016-09-01 18:10:52 -0600

    add log model to make logging easier

---
+++ uic-etl/models/dtos/LogModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using FluentValidation.Results;
+
+namespace uic_etl.models.dtos
+{
+    public class LogModel
+    {
+        public LogModel(string key, string id, Dictionary<string, IEnumerable<ValidationFailure>> errors)
+        {
+            Identifier = string.Format("{0} with the id: {1}", key, id);
+            Failures = errors;
+        }
+
+        public string Identifier { get; set; }
+        public Dictionary<string, IEnumerable<ValidationFailure>> Failures { get; set; }
+    }
+}
