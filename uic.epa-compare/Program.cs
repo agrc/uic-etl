16c48a76591de6d37ab2e18f21964b7ed72c20fb
steveoh <sgourley@utah.gov>
2016-12-28 12:09:42 -0700

    add comparisons for all elements

--- uic.epa-compare/Program.cs
+++ uic.epa-compare/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using Org.XmlUnit.Builder;
 using Org.XmlUnit.Diff;
-using uic.epa_compare.models;
 using uic.epa_compare.services;
+using CompareOptions = uic.epa_compare.models.CompareOptions;
 
 namespace uic.epa_compare
 {
@@ -41,7 +42,8 @@ namespace uic.epa_compare
 
             XDocument epa;
             XDocument uic;
-            var sortFacilities = new Func<XElement, string>(element => element.Elements().First().Elements().First().Value);
+            var nestedIdentifierSort = new Func<XElement, string>(element => element.Elements().First().Elements().First().Value);
+            var directIdentifierSort = new Func<XElement, string>(element => element.Elements().First().Value);
 
             using (var reader = File.OpenText(options.EpaFile))
             using (var reader2 = File.OpenText(options.UicEtlFile))
@@ -50,11 +52,23 @@ namespace uic.epa_compare
                 uic = XDocument.Load(reader2);
             }
 
-            var epaContainerNode = epa.Root.Descendants(Uic + "UIC");
-            var epaFacilityList = epaContainerNode.Elements(Uic + "FacilityList").OrderBy(sortFacilities);
+            var headingNode = Uic + "UIC";
+            var contactNode = Uic + "ContactDetail";
+            var permitNode = Uic + "PermitDetail";
+            var enforcementNode = Uic + "EnforcementDetail";
+            var facilityListNode = Uic + "FacilityList";
+            
+            var epaContainerNode = epa.Root.Descendants(headingNode);
+            var epaContacts = epaContainerNode.Elements(contactNode);
+            var epaPermits = epaContainerNode.Elements(permitNode);
+            var epaEnforcements = epaContainerNode.Elements(enforcementNode);
+            var epaFacilityList = epaContainerNode.Elements(facilityListNode);
 
-            var uicContainerNode = uic.Root.Descendants(Uic + "UIC");
-            var uicFacilities = uicContainerNode.Elements(Uic + "FacilityList").OrderBy(sortFacilities);
+            var uicContainerNode = uic.Root.Descendants(headingNode);
+            var uicContacts = uicContainerNode.Elements(contactNode);
+            var uicPermits = uicContainerNode.Elements(permitNode);
+            var uicEnforcements = uicContainerNode.Elements(enforcementNode);
+            var uicFacilities = uicContainerNode.Elements(facilityListNode);
 
             var skips = new[]
             {
@@ -63,11 +77,121 @@ namespace uic.epa_compare
 
             var selector = ElementSelectors.ConditionalBuilder()
                         .WhenElementIsNamed("WellInspectionDetail").ThenUse(ByNameAndTextRecSelector.CanBeCompared)
+                        .WhenElementIsNamed("MITestDetail").ThenUse(ByNameAndTextRecSelector.CanBeCompared)
                         .ElseUse(ElementSelectors.ByNameAndText)
                         .Build();
 
             var nodeMatcher = new DefaultNodeMatcher(selector);
 
+            foreach (var epaElement in epaContacts)
+            {
+                var element = epaElement.Elements().First();
+                var id = element.Value;
+                var idTag = element.Name;
+
+                var uicMatch = uicContacts
+                    .SingleOrDefault(x => x.Element(idTag).Value == id);
+
+                if (uicMatch == null)
+                {
+                    Console.WriteLine("Missing {1} element: {0}", id, idTag.LocalName);
+                    continue;
+                }
+
+                var myDiff = DiffBuilder.Compare(epaElement)
+                        .WithTest(uicMatch)
+                        .CheckForSimilar()
+                        .WithNodeMatcher(nodeMatcher)
+                        .WithNodeFilter(x => !skips.Contains(x.LocalName))
+                        .IgnoreComments()
+                        .IgnoreWhitespace()
+                        .NormalizeWhitespace()
+                        .Build();
+
+                if (myDiff.HasDifferences())
+                {
+                    Debug.WriteLine(id);
+                    foreach (var item in myDiff.Differences)
+                    {
+                        Debug.WriteLine(item.Comparison);
+                    }
+                }
+            }
+            Console.WriteLine("Done comparing global contacts");
+
+            Console.WriteLine("Comparing Permits");
+            foreach (var epaElement in epaPermits)
+            {
+                var element = epaElement.Elements().First();
+                var id = element.Value;
+                var idTag = element.Name;
+
+                var uicMatch = uicPermits
+                    .SingleOrDefault(x => x.Element(idTag).Value == id);
+
+                if (uicMatch == null)
+                {
+                    Console.WriteLine("Missing {1} element: {0}", id, idTag.LocalName);
+                    continue;
+                }
+
+                var myDiff = DiffBuilder.Compare(epaElement)
+                        .WithTest(uicMatch)
+                        .CheckForSimilar()
+                        .WithNodeMatcher(nodeMatcher)
+                        .WithNodeFilter(x => !skips.Contains(x.LocalName))
+                        .IgnoreComments()
+                        .IgnoreWhitespace()
+                        .NormalizeWhitespace()
+                        .Build();
+
+                if (myDiff.HasDifferences())
+                {
+                    Debug.WriteLine(id);
+                    foreach (var item in myDiff.Differences)
+                    {
+                        Debug.WriteLine(item.Comparison);
+                    }
+                }
+            }
+            Console.WriteLine("Done comparing global permits");
+
+            Console.WriteLine("Comparing Enforcements");
+            foreach (var epaElement in epaEnforcements)
+            {
+                var element = epaElement.Elements().First();
+                var id = element.Value;
+                var idTag = element.Name;
+
+                var uicMatch = uicEnforcements
+                    .SingleOrDefault(x => x.Element(idTag).Value == id);
+
+                if (uicMatch == null)
+                {
+                    Console.WriteLine("Missing {1} element: {0}", id, idTag.LocalName);
+                    continue;
+                }
+
+                var myDiff = DiffBuilder.Compare(epaElement)
+                        .WithTest(uicMatch)
+                        .CheckForSimilar()
+                        .WithNodeMatcher(nodeMatcher)
+                        .WithNodeFilter(x => !skips.Contains(x.LocalName))
+                        .IgnoreComments()
+                        .IgnoreWhitespace()
+                        .NormalizeWhitespace()
+                        .Build();
+
+                if (myDiff.HasDifferences())
+                {
+                    Debug.WriteLine(id);
+                    foreach (var item in myDiff.Differences)
+                    {
+                        Debug.WriteLine(item.Comparison);
+                    }
+                }
+            }
+            Console.WriteLine("Done comparing global enforcements");
             // loop over facility list items
             foreach (var epaFacilityListItem in epaFacilityList)
             {
