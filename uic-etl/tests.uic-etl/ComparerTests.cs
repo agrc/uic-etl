f508c04bf8e91db57155c496ffb00ba14d91e79c
steveoh <sgourley@utah.gov>
2016-12-29 12:46:17 -0700

    add test for equality comparer

---
+++ uic-etl/tests.uic-etl/ComparerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using domain.uic_etl.xml;
+using uic_etl.comparers;
+using Xunit;
+
+namespace tests.uic_etl
+{
+    public class ComparerTests
+    {
+        [Fact]
+        public void HashSetWithReferenceObject()
+        {
+            var hash = new HashSet<ResponseDetail>(new ResponseDetailComparer());
+
+            var responseOne = new ResponseDetail
+            {
+                EnforcementActionDate = "yyyyMMdd",
+                EnforcementActionType = "INF",
+                ResponseEnforcementIdentifier = "ID1",
+                ResponseViolationIdentifier = "FID1"
+            };
+
+            hash.Add(responseOne);
+
+            var responseTwo = new ResponseDetail
+            {
+                EnforcementActionDate = "yyyyMMdd",
+                EnforcementActionType = "INF",
+                ResponseEnforcementIdentifier = "ID1",
+                ResponseViolationIdentifier = "FID1"
+            };
+
+            hash.Add(responseTwo);
+
+            Assert.Equal(1, hash.Count);
+        }
+    }
+}
